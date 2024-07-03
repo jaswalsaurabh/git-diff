591a9a31f48087943cc369499a19e71c73a4d63d
saurabh-jaswal <jaswalsaurabhjnv@gmail.com>
2024-07-03 10:35:25 +0530

    removed local mock data and call github api

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,49 +6,55 @@ const API_URL = "https://api.github.com/repos/";
 
 const commitInfoCache = {};
 
-
-router.get('/',(req,res)=>{
+router.get("/", (req, res) => {
   try {
     res.send("Hello from git diff server");
   } catch (error) {
     res.status(400).send("Something is wrong");
   }
-})
-
-router.get("/repositories/:owner/:repository/commits/:oid", async (req, res) => {
-  try {
-    const { owner, repository, oid } = req.params;
-    let data;
-    // if (!commitInfoCache[oid]) {
-    //   const response = await fetch(
-    //     API_URL + `${owner}/${repository}/commits/${oid}`
-    //   );
-
-    //   data = await response.json();
-    //   commitInfoCache[oid] = data;
-    // } else {
-    //   data = commitInfoCache[oid];
-    // }
-    // 
-    data = commitData;
-    commitInfoCache[oid] = data;
-    // 
-    const newData = parseCommit(data);
-    res.status(200).json([...newData]);
-  } catch (error) {
-    res.status(400).json({ message: "Something went wrong", error });
-  }
 });
 
-router.get("/repositories/:owner/:repository/commits/:oid/diff", async (req, res) => {
-  try {
-    const { oid } = req.params;
-    const commitData = commitInfoCache[oid];
-    const newData = transformCommitData(commitData);
-    res.status(200).json([...newData]);
-  } catch (error) {
-    res.status(400).json({ message: "Something went wrong", error });
+router.get(
+  "/repositories/:owner/:repository/commits/:oid",
+  async (req, res) => {
+    try {
+      const { owner, repository, oid } = req.params;
+      let data;
+      if (!commitInfoCache[oid]) {
+        const response = await fetch(
+          API_URL + `${owner}/${repository}/commits/${oid}`
+        );
+
+        data = await response.json();
+        commitInfoCache[oid] = data;
+      } else {
+        data = commitInfoCache[oid];
+      }
+
+      // mock data start
+      // data = commitData;
+      // commitInfoCache[oid] = data;
+      // mock data end
+      const newData = parseCommit(data);
+      res.status(200).json([...newData]);
+    } catch (error) {
+      res.status(400).json({ message: "Something went wrong", error });
+    }
   }
-});
+);
+
+router.get(
+  "/repositories/:owner/:repository/commits/:oid/diff",
+  async (req, res) => {
+    try {
+      const { oid } = req.params;
+      const commitData = commitInfoCache[oid];
+      const newData = transformCommitData(commitData);
+      res.status(200).json([...newData]);
+    } catch (error) {
+      res.status(400).json({ message: "Something went wrong", error });
+    }
+  }
+);
 
 module.exports = { router };
